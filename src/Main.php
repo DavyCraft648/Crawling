bd0ba6f658b7766232ecb3adbe9eef200c7cd695
DavyCraft648 <67502532+DavyCraft648@users.noreply.github.com>
2023-07-14 15:15:04 +0700

    Update
    #1

--- src/Main.php
+++ src/Main.php
@@ -6,8 +6,11 @@ namespace DavyCraft648\Crawling;
 use pocketmine\event\EventPriority;
 use pocketmine\event\player\PlayerQuitEvent;
 use pocketmine\event\server\DataPacketReceiveEvent;
+use pocketmine\event\server\DataPacketSendEvent;
 use pocketmine\network\mcpe\protocol\PlayerAuthInputPacket;
-use pocketmine\network\mcpe\protocol\types\PlayerAuthInputFlags;
+use pocketmine\network\mcpe\protocol\StartGamePacket;
+use pocketmine\network\mcpe\protocol\types\Experiments;
+use pocketmine\network\mcpe\protocol\types\PlayerAuthInputFlags as Flags;
 
 class Main extends \pocketmine\plugin\PluginBase{
 
@@ -17,6 +20,15 @@ class Main extends \pocketmine\plugin\PluginBase{
 	private array $crawlingPlayers = [];
 
 	protected function onEnable() : void{
+		$this->getServer()->getPluginManager()->registerEvent(DataPacketSendEvent::class, function(DataPacketSendEvent $event) : void{
+			foreach($event->getPackets() as $packet){
+				if($packet instanceof StartGamePacket){
+					$packet->levelSettings->experiments = new Experiments(array_merge($packet->levelSettings->experiments->getExperiments(), [
+						"short_sneaking" => true
+					]), true);
+				}
+			}
+		}, EventPriority::HIGHEST, $this);
 		$this->getServer()->getPluginManager()->registerEvent(DataPacketReceiveEvent::class, function(DataPacketReceiveEvent $event) : void{
 			$packet = $event->getPacket();
 			if($packet instanceof PlayerAuthInputPacket){
@@ -26,10 +38,16 @@ class Main extends \pocketmine\plugin\PluginBase{
 					$inputFlags = $packet->getInputFlags();
 					if(!isset($this->lastPlayerAuthInputFlags[$playerName]) || $inputFlags !== $this->lastPlayerAuthInputFlags[$playerName]){
 						$this->lastPlayerAuthInputFlags[$playerName] = $inputFlags;
-						if((($inputFlags & (1 << 40)) !== 0) !== (($inputFlags & (1 << 41)) !== 0)){
-							(new \ReflectionProperty($packet, "inputFlags"))->setValue($packet, ($this->crawlingPlayers[$playerName] = ($inputFlags & (1 << 40)) !== 0) ? $inputFlags | (1 << PlayerAuthInputFlags::START_SWIMMING) : $inputFlags | (1 << PlayerAuthInputFlags::STOP_SWIMMING));
-						}elseif(($this->crawlingPlayers[$playerName] ?? false) && ($inputFlags & (1 << PlayerAuthInputFlags::START_SWIMMING)) === 0){
-							(new \ReflectionProperty($packet, "inputFlags"))->setValue($packet, $inputFlags | (1 << PlayerAuthInputFlags::START_SWIMMING));
+						$isStartCrawling = ($inputFlags & (1 << Flags::START_CRAWLING)) !== 0;
+						$isStopCrawling = ($inputFlags & (1 << Flags::STOP_CRAWLING)) !== 0;
+						if($isStartCrawling !== $isStopCrawling){
+							(new \ReflectionProperty($packet, "inputFlags"))->setValue($packet, ($this->crawlingPlayers[$playerName] = $isStartCrawling) ? $inputFlags | (1 << Flags::START_SWIMMING) : $inputFlags | (1 << Flags::STOP_SWIMMING));
+						}elseif(($this->crawlingPlayers[$playerName] ?? false) && ($inputFlags & (1 << Flags::START_SWIMMING)) === 0){
+							if($player->isSpectator() || ($player->getEyeHeight() > 1 && !$player->getPosition()->getWorld()->getBlock($player->getEyePos())->collidesWithBB($player->getBoundingBox()))){
+								unset($this->crawlingPlayers[$playerName]);
+								return;
+							}
+							(new \ReflectionProperty($packet, "inputFlags"))->setValue($packet, $inputFlags | (1 << Flags::START_SWIMMING));
 						}
 					}
 				}
